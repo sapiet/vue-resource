1e3f20f2e3e37fcb39e09ff64dd91cbd75d35cb7
Xavier QUIÈVRE <xavier@synapse-interactive.fr>
2019-09-23 16:21:28 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Vue Resource</h3>
+
+    <div class="ui active dimmer" v-if="loading">
+      <div class="ui loader"></div>
+    </div>
+
+    <div class="ui four column grid">
+      <div class="column" v-for="user in users">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">
+              <input type="text" v-model="user.name" style="width: 100%">
+            </div>
+            <div class="meta">
+              <a href="">Friends</a>
+            </div>
+            <div class="description">
+              {{ user.address.street }}<br>
+              {{ user.address.zipcode }} {{ user.address.city }}<br>
+            </div>
+          </div>
+          <div class="extra content">
+            <div class="ui two buttons">
+              <button class="ui basic green button" @click="edit(user)">
+                Edit
+              </button>
+              <button class="ui basic red button" @click="remove(user)">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const ENDPOINT = 'users{/id}';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      loading: false,
+      users: []
+    }
+  },
+  mounted() {
+    this.$user = this.$resource(ENDPOINT, {}, {}, {
+      before: () => this.loading = true,
+      after: () => this.loading = false,
+    });
+
+    this.$user.query(ENDPOINT).then(
+      response => this.users = response.data,
+      error => window.alert(error)
+    )
+  },
+  methods: {
+    edit(user) {
+      this.$user.update({id: user.id}, {
+        name: user.name
+      }).then(
+        response => {},
+        error => window.alert(error)
+      )
+    },
+
+    remove(user) {
+      this.$user.remove({id: user.id}).then(
+        response => this.users.splice(this.users.indexOf(user), 1),
+        error => window.alert(error)
+      )
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueResource);
+Vue.http.options.root = 'https://jsonplaceholder.typicode.com';
+
+Vue.http.interceptors.push((request, next) => {
+  next(response => {
+    if (request.after) {
+      request.after.call(this, request);
+    }
+  });
+});
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
